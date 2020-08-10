1757cee7fe01c647bc274fb2f4cb029b39fe643e
keonlee9420 <rjsdlwwkd@kaist.ac.kr>
2020-08-10 15:36:27 +0900

    modules for the decoder

--- decoder.py
+++ decoder.py
@@ -4,7 +4,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
-from encoder import clones, LayerNorm, SublayerConnection
+from encoder import clones, LayerNorm, ConvNorm, SublayerConnection
 
 
 class Decoder(nn.Module):
@@ -59,3 +59,101 @@ def greedy_decode(model, src, src_mask, max_len, start_symbol):
         ys = torch.cat([ys,
                         torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
     return ys
+
+
+class DecoderPrenet(nn.Module):
+    """
+    Decoder pre-net for TTS
+    For the alignment between phoneme and mel frame to be measured
+    """
+
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout):
+        super(DecoderPrenet, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        self.layer = nn.Sequential(
+            nn.Linear(self.input_dim, self.hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.Linear(self.hidden_dim, self.output_dim),
+            nn.ReLU(),
+            nn.Dropout(p=dropout)
+        )
+
+    def forward(self, decoder_input):
+        return self.layer(decoder_input)
+
+
+class MelLinear(nn.Module):
+    """Linear projections to predict the mel spectrogram same as Tacotron2."""
+
+    def __init__(self, num_hidden, mel_channels):
+        super(MelLinear, self).__init__()
+        self.mel_linear = nn.Linear(num_hidden, mel_channels)
+
+    def forward(self, decoder_input):
+        return self.mel_linear(decoder_input)
+
+
+class StopLinear(nn.Module):
+    """Linear projections to predict the stop token same as Tacotron2."""
+
+    def __init__(self, num_hidden):
+        super(StopLinear, self).__init__()
+        self.stop_linear = nn.Linear(num_hidden, 1, w_init='sigmoid')
+
+    def forward(self, decoder_input):
+        return self.stop_linear(decoder_input)
+
+
+class Postnet(nn.Module):
+    """Linear projections to produce a residual to refine the reconstruction of mel spectrogram same as Tacotron2."""
+
+    def __init__(self, mel_channels, hidden_dim, kernel_size, num_conv=5, dropout=0.1):
+        super(Postnet, self).__init__()
+        self.mel_channels = mel_channels
+        self.hidden_dim = hidden_dim
+        self.kernel_size = kernel_size
+        self.num_conv = num_conv
+
+        self.dropout = nn.Dropout(dropout)
+        self.convolutions = nn.ModuleList()
+
+        self.convolutions.append(
+            nn.Sequential(
+                ConvNorm(self.mel_channels, self.hidden_dim,
+                         kernel_size=self.kernel_size, stride=1,
+                         padding=int((self.kernel_size - 1) / 2),
+                         dilation=1, w_init_gain='tanh'),
+                nn.BatchNorm1d(self.hidden_dim))
+        )
+
+        for i in range(1, self.num_conv - 1):
+            self.convolutions.append(
+                nn.Sequential(
+                    ConvNorm(self.hidden_dim,
+                             self.hidden_dim,
+                             kernel_size=self.kernel_size, stride=1,
+                             padding=int(
+                                 (self.kernel_size - 1) / 2),
+                             dilation=1, w_init_gain='tanh'),
+                    nn.BatchNorm1d(self.hidden_dim))
+            )
+
+        self.convolutions.append(
+            nn.Sequential(
+                ConvNorm(self.hidden_dim, self.mel_channels,
+                         kernel_size=self.kernel_size, stride=1,
+                         padding=int((self.kernel_size - 1) / 2),
+                         dilation=1, w_init_gain='linear'),
+                nn.BatchNorm1d(self.mel_channels))
+        )
+
+    def forward(self, x):
+        for i in range(len(self.convolutions) - 1):
+            x = self.dropout(torch.tanh(self.convolutions[i](x)))
+        x = self.dropout(self.convolutions[-1](x))
+
+        return x
--- encoder.py
+++ encoder.py
@@ -22,6 +22,27 @@ class LayerNorm(nn.Module):
         std = x.std(-1, keepdim=True)
         return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
 
+class ConvNorm(nn.Module):
+    """Construct a convnorm module. (from tacotron2)"""
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1,
+                 padding=None, dilation=1, bias=True, w_init_gain='linear'):
+        super(ConvNorm, self).__init__()
+        if padding is None:
+            assert(kernel_size % 2 == 1)
+            padding = int(dilation * (kernel_size - 1) / 2)
+
+        self.conv = nn.Conv1d(in_channels, out_channels,
+                                    kernel_size=kernel_size, stride=stride,
+                                    padding=padding, dilation=dilation,
+                                    bias=bias)
+
+        nn.init.xavier_uniform_(
+            self.conv.weight, gain=nn.init.calculate_gain(w_init_gain))
+
+    def forward(self, signal):
+        conv_signal = self.conv(signal)
+        return conv_signal
+
 class Encoder(nn.Module):
     """Core encoder is a stack of N layers"""
     def __init__(self, layer, N):
